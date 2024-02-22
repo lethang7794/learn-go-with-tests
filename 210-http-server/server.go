f84b7eccabcb819b8d2794d104b4d37a26ac04b3
Le Quang Thang <lethang7794@gmail.com>
2024-02-22 15:58:07 +0700

    [Red] Handle Post method

--- 210-http-server/server.go
+++ 210-http-server/server.go
@@ -12,6 +12,16 @@ type PlayerHandler struct {
 
 func (p *PlayerHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	player := strings.TrimPrefix(request.URL.Path, "players/")
+
+	if request.Method == http.MethodGet {
+		p.ShowScore(writer, player)
+	}
+	if request.Method == http.MethodPost {
+		p.ProcessWin(writer, player)
+	}
+}
+
+func (p *PlayerHandler) ShowScore(writer http.ResponseWriter, player string) {
 	score, ok := p.store.GetPlayerScore(player)
 	if !ok {
 		writer.WriteHeader(http.StatusNotFound)
