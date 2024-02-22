4db89aceab56d674c9a94a73b76823295b4f72a7
Le Quang Thang <lethang7794@gmail.com>
2024-02-22 13:46:34 +0700

    Refactor - Split Arrange, Act, Assert; inline got, want

--- 210-http-server/server_test.go
+++ 210-http-server/server_test.go
@@ -10,23 +10,19 @@ func TestGETPlayers(t *testing.T) {
 	t.Run("return Pepper score", func(t *testing.T) {
 		response := httptest.NewRecorder()
 		request := newGetScoreRequest("Pepper")
+
 		GetPlayerHandler(response, request)
 
-		got := response.Body.String()
-		want := "20"
-
-		assertResponseBody(t, got, want)
+		assertResponseBody(t, response.Body.String(), "20")
 	})
 
 	t.Run("return Floyd score", func(t *testing.T) {
 		response := httptest.NewRecorder()
 		request := newGetScoreRequest("Floyd")
+
 		GetPlayerHandler(response, request)
 
-		got := response.Body.String()
-		want := "10"
-
-		assertResponseBody(t, got, want)
+		assertResponseBody(t, response.Body.String(), "10")
 	})
 }
 
