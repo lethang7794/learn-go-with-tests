96a1304254581913e404c26f4545a7e563b9d8ea
Le Quang Thang <lethang7794@gmail.com>
2024-02-22 15:00:18 +0700

    Make the main app work

--- 210-http-server/main.go
+++ 210-http-server/main.go
@@ -6,9 +6,17 @@ import (
 )
 
 func main() {
-	handler := &PlayerHandler{}
+	store := &InMemoryPlayerStore{}
+	handler := &PlayerHandler{store}
 	err := http.ListenAndServe(":5000", handler)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type InMemoryPlayerStore struct {
+}
+
+func (f *InMemoryPlayerStore) GetPlayerScore(player string) int {
+	return 12345
 }
