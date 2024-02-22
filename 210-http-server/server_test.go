375bc16b56ed33b970518018d9474417cb9f843e
Le Quang Thang <lethang7794@gmail.com>
2024-02-22 13:34:08 +0700

    Refactor - Add test helpers

--- 210-http-server/server_test.go
+++ 210-http-server/server_test.go
@@ -9,27 +9,35 @@ import (
 func TestGETPlayers(t *testing.T) {
 	t.Run("return Pepper score", func(t *testing.T) {
 		response := httptest.NewRecorder()
-		request, _ := http.NewRequest(http.MethodGet, "players/Pepper", nil)
+		request := newGetScoreRequest("Pepper")
 		GetPlayerHandler(response, request)
 
 		got := response.Body.String()
 		want := "20"
 
-		if got != want {
-			t.Errorf("got %#v, want %#v", got, want)
-		}
+		assertResponseBody(t, got, want)
 	})
 
 	t.Run("return Floyd score", func(t *testing.T) {
 		response := httptest.NewRecorder()
-		request, _ := http.NewRequest(http.MethodGet, "players/Floyd", nil)
+		request := newGetScoreRequest("Floyd")
 		GetPlayerHandler(response, request)
 
 		got := response.Body.String()
 		want := "10"
 
-		if got != want {
-			t.Errorf("got %#v, want %#v", got, want)
-		}
+		assertResponseBody(t, got, want)
 	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, "players/"+name, nil)
+	return request
+}
+
+func assertResponseBody(t *testing.T, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("wrong response body: got %#v, want %#v", got, want)
+	}
 }
