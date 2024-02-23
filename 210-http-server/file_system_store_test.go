9412db715dde2eeddf9f947e3b34b886b551a176
Le Quang Thang <lethang7794@gmail.com>
2024-02-23 15:32:36 +0700

    [Refactor] Move FileSystemPlayerStore to another file

--- 210-http-server/file_system_store_test.go
+++ 210-http-server/file_system_store_test.go
@@ -1,48 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"io"
-	"os"
 	"testing"
 )
-
-type FileSystemPlayerStore struct {
-	database io.ReadWriteSeeker
-}
-
-func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStore {
-	return &FileSystemPlayerStore{database: database}
-}
-
-func (f FileSystemPlayerStore) GetLeague() League {
-	f.database.Seek(0, 0)
-	league, _ := NewLeague(f.database)
-	return league
-}
-
-func (f FileSystemPlayerStore) GetPlayerScore(name string) (int, bool) {
-	league := f.GetLeague()
-	player := league.Find(name)
-	if player != nil {
-		return player.Score, true
-	}
-	return 0, false
-}
-
-func (f FileSystemPlayerStore) RecordWin(name string) {
-	league := f.GetLeague()
-	player := league.Find(name)
-	if player != nil {
-		player.Score++
-	} else {
-		league = append(league,
-			Player{Name: name, Score: 1},
-		)
-	}
-	f.database.Seek(0, 0)
-	json.NewEncoder(f.database).Encode(league)
-}
 
 func TestFileSystemStore(t *testing.T) {
 	database, cleanup := createTempFile(t, `[
@@ -93,19 +53,4 @@ func TestFileSystemStore(t *testing.T) {
 			t.Errorf("got %#v, want %#v", got, want)
 		}
 	})
-}
-
-func createTempFile(t *testing.T, initialData string) (_ io.ReadWriteSeeker, cleanup func()) {
-	temp, err := os.CreateTemp("", "db")
-	if err != nil {
-		t.Fatalf("failed creating temp file: %v", err)
-	}
-	_, err = temp.Write([]byte(initialData))
-	if err != nil {
-		t.Fatalf("failed writing initial data: %v", err)
-	}
-	return temp, func() {
-		temp.Close()
-		os.Remove(temp.Name())
-	}
 }
