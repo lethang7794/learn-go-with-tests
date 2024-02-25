12e1be455a67915aca8a81275001ff7ab694b925
Le Quang Thang <lethang7794@gmail.com>
2024-02-25 14:56:18 +0700

    red: record correct winner

--- cli_test.go
+++ cli_test.go
@@ -1,16 +1,27 @@
 package poker
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestCLI(t *testing.T) {
-	t.Run("win call once", func(t *testing.T) {
+	t.Run("win call once & record correct winner", func(t *testing.T) {
+		winner := "Andy"
+		input := fmt.Sprintf("%s wins", winner)
+		in := strings.NewReader(input)
 		store := &StubPlayerStore{}
-		cli := &CLI{store}
+		cli := &CLI{store, in}
 
 		cli.PlayPoker()
 
 		if len(store.winCalls) != 1 {
 			t.Errorf("want win to be called once, but it's called %v times (%v)", len(store.winCalls), store.winCalls)
+		}
+
+		if store.winCalls[0] != winner {
+			t.Errorf("got %#v, want %#v", store.winCalls[0], winner)
 		}
 	})
 }
