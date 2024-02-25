767c4c1ce1652ec9374a89fea35da4ae94f97812
Le Quang Thang <lethang7794@gmail.com>
2024-02-25 15:20:18 +0700

    refactor: extract logic get winner

--- cli.go
+++ cli.go
@@ -15,6 +15,11 @@ func (c *CLI) PlayPoker() {
 	reader := bufio.NewScanner(c.in)
 	reader.Scan()
 	line := reader.Text()
-	winner := strings.Replace(line, " wins", "", 1)
+	winner := extractWinner(line)
 	c.store.RecordWin(winner)
+}
+
+func extractWinner(line string) string {
+	winner := strings.Replace(line, " wins", "", 1)
+	return winner
 }
