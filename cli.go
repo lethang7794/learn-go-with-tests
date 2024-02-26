b0936ec1ced4396a8f5a67793ebbe0a67373acb9
Le Quang Thang <lethang7794@gmail.com>
2024-02-26 10:33:42 +0700

    green: schedule blind at different times

--- cli.go
+++ cli.go
@@ -22,7 +22,13 @@ func NewCLI(store PlayerStore, in io.Reader, alerter Alerter) *CLI {
 }
 
 func (c *CLI) PlayPoker() {
-	c.alerter.ScheduleAlertAt(5*time.Second, 100)
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		c.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime += 10 * time.Minute
+	}
+
 	line := c.readLine()
 	winner := extractWinner(line)
 	c.store.RecordWin(winner)
