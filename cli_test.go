7d43e5487acae7e9ab4270201320c69ae3bb8fbb
Le Quang Thang <lethang7794@gmail.com>
2024-02-26 14:23:40 +0700

    red: expected error when user enter a non-numeric value

--- cli_test.go
+++ cli_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	t.Run("its prompt the user to enter the number of players & starts the game", func(t *testing.T) {
+	t.Run("prompt the user to enter the number of players & starts the game", func(t *testing.T) {
 		in := strings.NewReader("7\n")
 		out := &bytes.Buffer{}
 		gameSpy := &GameSpy{}
@@ -24,6 +24,19 @@ func TestCLI(t *testing.T) {
 		if gameSpy.StartsWith != 7 {
 			t.Errorf("got %#v, want %#v", gameSpy.StartsWith, 7)
 
+		}
+	})
+
+	t.Run("when users enter non numeric value, it print out an error & doesn't start the game", func(t *testing.T) {
+		in := strings.NewReader("Not a number\n")
+		out := &bytes.Buffer{}
+		gameSpy := &GameSpy{}
+		cli := NewCLI(in, out, gameSpy)
+
+		cli.PlayPoker()
+
+		if gameSpy.Started == true {
+			t.Errorf("Expected game not started, but it does")
 		}
 	})
 }
